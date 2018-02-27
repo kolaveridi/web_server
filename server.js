ded03a0394ea1f1db3eb120e28be18e3cbbf3b1d
kolaveridi <coderboysatya@gmail.com>
2018-02-28 01:41:28 +0530

    Add project page

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.get('/',(req,res)=>{
 
   });
 });
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
 app.get('/bad',(req,res)=>{
   res.send({
    errorMessage:'Unable to handle '
