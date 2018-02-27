e85256ae94b2515c8f8aaac4b247c38b1c350343
kolaveridi <coderboysatya@gmail.com>
2018-02-28 00:39:56 +0530

    setup start script

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;
+
 var app=express();
 app.set('view engine','hbs');
 hbs.registerPartials(__dirname+ '/views/partials');
@@ -38,4 +40,4 @@ app.get('/bad',(req,res)=>{
    errorMessage:'Unable to handle '
  });
 });
-app.listen(3000);
+app.listen(port);
